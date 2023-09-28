174d17e6ce9bc96215def2ff62c9766e7b68fdb4
MightyMahesh222 <mahesh18311m026@gmail.com>
2023-09-28 11:46:46 +0000

    updated code

--- app.js
+++ app.js
@@ -71,9 +71,105 @@ const authenticateToken = (request, response, next) => {
   }
 };
 // ALL STATES //
-app.get("/states/", authenticateToken, async (request, response) => {
+const toCamelCase = (snake_case) => {
+  return {
+    stateId: snake_case.state_id,
+    stateName: snake_case.state_name,
+    population: snake_case.population,
+  };
+};
+
+app.get("/states/", async (request, response) => {
   const sqlQuery = `select * from state;`;
   const getData = await db.all(sqlQuery);
+  response.send(getData.map((every) => toCamelCase(every)));
+});
+
+//GET STATE//
+app.get("/states/:stateId/", async (request, response) => {
+  const { stateId } = request.params;
+  const sqlQuery = `select * from state where state_id='${stateId}'`;
+  const getData = await db.get(sqlQuery);
+  response.send(toCamelCase(getData));
+});
+
+//POST DISTRICT//
+app.post("/districts", async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const SqlQuery = `insert into district (district_name,state_id,cases,cured,active,deaths)
+                        values(
+                            '${districtName}',
+                            '${stateId}',
+                            '${cases}',
+                            '${cured}',
+                            '${active}',
+                            '${deaths}'
+                        )`;
+  const insertData = await db.run(SqlQuery);
+  response.send("District Successfully Added");
+});
+
+//GET DISTRICT//
+const toTheCamelCase = (snake_case) => {
+  return {
+    districtId: snake_case.district_id,
+    districtName: snake_case.district_name,
+    stateId: snake_case.state_id,
+    cases: snake_case.cases,
+    cured: snake_case.cured,
+    active: snake_case.active,
+    deaths: snake_case.deaths,
+  };
+};
+
+app.get("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const sqlQuery = `select * from district where district_id='${districtId}'`;
+  const getData = await db.get(sqlQuery);
+  response.send(toTheCamelCase(getData));
+});
+
+//DELETE DISTRICT//
+
+app.delete("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const sqlQuery = `DELETE from district where district_id='${districtId}'`;
+  const getData = await db.run(sqlQuery);
+  response.send("District Removed");
+});
+
+//UPDATE DISTRICT//
+app.put("/districts/:districtId/", async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const { districtId } = request.params;
+  const sqlQuery = `UPDATE district SET
+                    district_name='${districtName}',
+                     state_id='${stateId}',
+                      cases='${cases}',
+                       cured='${cured}',
+                        active='${active}',
+                         deaths='${deaths}'                    
+                     where district_id='${districtId}'`;
+  const getData = await db.run(sqlQuery);
+  response.send("District Details Updated");
+});
+
+//SAMPLE//
+app.get("/alldistricts/", async (request, response) => {
+  const sqlQuery = `select * from district;`;
+  const getData = await db.all(sqlQuery);
+  response.send(getData);
+});
+
+//STATS//
+app.get("/states/:stateId/stats/", async (request, response) => {
+  const { stateId } = request.params;
+  const sqlQuery = `select sum(cases) as totalCases,
+                             sum(cured) as totalCured,
+                             sum(active) as totalActive,
+                             sum(deaths) as totalDeaths
+  from district where state_id='${stateId}'`;
+  const getData = await db.get(sqlQuery);
   response.send(getData);
 });
 
